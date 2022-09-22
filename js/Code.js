9d224ea1ac92036b73c1017dc1b7ea3f18d620a9
maxg1007 <90800419+maxg1007@users.noreply.github.com>
2022-09-22 17:58:09 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: SEMPRE MUDANDO, NÃO CONSTANTE!", 100, 70);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: PERFORMA UMA TAREFA EM PARTICULAR!", 580, 170);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: ARMAZENA TODAS AS INFORMAÇÕES!", 80, 270);
+
+  // Adicione código para exibir as outras duas perguntas.
+}
---
+++ js/System.js
@@ -0,0 +1,15 @@
+class System {
+  constructor() {}
+
+  authenticate(actualCode, enteredCode) {
+  
+    if(actualCode===enteredCode){
+      return  true
+    }else{
+      return false
+    }
+
+    // Adicionar código para "return true" se a resposta correta for inserida senão "return false".
+    
+  }
+}
